e762333ec750dca419e1b029862e9f7e75dfb27b
Oğuz Sert <oguzsert@yandex.com.tr>
2017-07-14 15:52:30 +0300

    'socketimplementasyonu'

--- server.js
+++ server.js
@@ -9,6 +9,39 @@ io.on('connection', function(client){
 		players[player.id] = player;		
 		client.broadcast.emit("newPlayerJoined",player);
 	});
+	
+	client.on("engine-on",function(playerId){		
+		client.broadcast.emit("player-engine-on",playerId);
+	});
+	
+	client.on("engine-off",function(playerId){		
+		client.broadcast.emit("player-engine-off",playerId);
+	});
+	
+	client.on("move-forward",function(playerId){		
+		client.broadcast.emit("player-move-forward",playerId);
+	});
+	
+	client.on("move-backward",function(playerId){		
+		client.broadcast.emit("player-move-backward",playerId);
+	});
+	
+	client.on("stop-movement",function(playerId){		
+		client.broadcast.emit("player-stop-movement",playerId);
+	});
+	
+	client.on("rotate-right",function(playerId){		
+		client.broadcast.emit("player-rotate-right",playerId);
+	});
+	
+	client.on("rotate-left",function(playerId){		
+		client.broadcast.emit("player-rotate-left",playerId);
+	});
+	
+	client.on("stop-rotate",function(playerId){		
+		client.broadcast.emit("player-rotate-stop",playerId);
+	});
+  
   
 });
 server.listen(3000);
